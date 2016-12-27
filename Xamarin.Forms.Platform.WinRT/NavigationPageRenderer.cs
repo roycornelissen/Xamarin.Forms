396bef57178bc6699a98fe804e3c7b1324e33054
E.Z. Hart <hartez@gmail.com>
2016-12-27 15:25:43 -0700

    Simplify GetCommandBarAsync, remove unneeded comments

--- Xamarin.Forms.Platform.WinRT/NavigationPageRenderer.cs
+++ Xamarin.Forms.Platform.WinRT/NavigationPageRenderer.cs
@@ -177,10 +177,7 @@ namespace Xamarin.Forms.Platform.WinRT
 				LookupRelevantParents();
 				UpdateTitleColor();
 				UpdateNavigationBarBackground();
-
-#if WINDOWS_UWP
 				UpdateToolbarPlacement();
-#endif
 
 				Element.PropertyChanged += OnElementPropertyChanged;
 				((INavigationPageController)Element).PushRequested += OnPushRequested;
